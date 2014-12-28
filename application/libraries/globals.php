6ff51cf26d6426f3b4ed3c939d3d3f295b950fce
Chris Evans <chris.evans@balanceinternet.com.au>
2014-12-28 22:31:38 +1100

    limited title in footer deals to be 47 chars

--- application/libraries/globals.php
+++ application/libraries/globals.php
@@ -115,12 +115,13 @@ class globals {
 		
 		foreach($listing as $list)
 		{
+			$this->load->helper('text');
 			// width="279"
 			$result .='<a href="/deal/'.str_replace(" ","-",$list[0]->search_engine_name).'">';
 			$result .='<div class="feat_item">';
             $result .='<img src="'.$list[0]->image.'" width="129"   />';
             $result .='<div class="feat_sup_content">';
-    	    $result .='<h2>'.$list[0]->bucket_list_name.'</h2>';
+    	    $result .='<h2>'.word_limiter($list[0]->bucket_list_name,47).'</h2>';
             
             $result .='</div>';
 			$result .='</div>';
