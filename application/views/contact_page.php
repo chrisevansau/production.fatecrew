32e00df679107eb8f9382388ef7b7102c3344948
Chris Evans <chris.evans@balanceinternet.com.au>
2015-01-01 18:11:16 +1100

    updated contact copy

--- application/views/contact_page.php
+++ application/views/contact_page.php
@@ -8,7 +8,8 @@
 			<h3>
 				we would love to hear from you.
 			</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In fermentum fermentum est, lacinia fermentum nibh commodo vel. Integer a est tellus. Integer eu libero tellus. Integer lobortis leo at dolor pharetra sit amet cursus mi fermentum. Praesent vel libero est. Ut dictum enim ut nulla adipiscing vitae faucibus lectus placerat. Mauris vel erat neque. Aliquam lacinia quam ac magna convallis porttitor. Curabitur odio quam, hendrerit et facilisis ut, adipiscing in lacus. Aenean lobortis massa iaculis justo consequat aliquet. Nunc id nisl lectus, et tempor est. Nullam volutpat viverra metus eu eleifend. Sed convallis rutrum tellus et consectetur. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam id quam tellus. </p>
+			<p>Need to get in touch? Drop us a line below in the contact form and we’ll get back to you shortly. You can also try our FAQs page with many helpful tips and tricks.  </p>
+			<p>Don’t forget to add us on your favourite social network. You can see the links to all our networks in the footer at the bottom of the page.</p>
 		</div>
 	</div>
 </div>
