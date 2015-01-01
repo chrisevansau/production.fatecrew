89c9afaadd7f0b2cb057c9191986e26f2c11316a
Chris Evans <chris.evans@balanceinternet.com.au>
2015-01-01 16:49:09 +1100

    updated T&C page

--- application/views/terms_and_conditions.php
+++ application/views/terms_and_conditions.php
@@ -1,21 +1,81 @@
   <div class="contnet">
   <div class="two_third">
     <h1>Terms and conditions</h1>
-  
+
+<p><strong>In using this website you are deemed to have read and agreed to the following terms and conditions:</strong></p>
+
+<p>The following terminology applies to&nbsp;these Terms and Conditions, Privacy Statement and Disclaimer Notice and any or all Agreements:&nbsp;&quot;Client&quot;, &ldquo;You&rdquo; and &ldquo;Your&rdquo; refers to you, the person accessing this website and accepting the Company&rsquo;s terms and conditions. &quot;The Company&quot;, &ldquo;Ourselves&rdquo;, &ldquo;We&rdquo; and &quot;Us&quot;, refers to our Company. &ldquo;Party&rdquo;, &ldquo;Parties&rdquo;, or &ldquo;Us&rdquo;, refers to both the Client and ourselves, or either the Client or ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the&nbsp;most appropriate manner, whether by formal meetings of a fixed duration, or any other means, for the express purpose of meeting the Client&rsquo;s needs in respect of provision of the Company&rsquo;s stated services/products, in accordance with and subject to, prevailing English Law. Any use of the above terminology or other words in the singular, plural, capitalisation and/or&nbsp;he/she or they, are taken as interchangeable and therefore as referring to same.</p>
+
+<h2>Privacy Statement</h2>
+ <p> We are committed to protecting your privacy. Authorized employees within the company on a need to know basis only use any information collected from individual customers. We constantly review our systems and data to ensure the best possible service to our customers. Parliament has created specific offences for unauthorised actions against computer systems and data. We will investigate any such actions with a view to prosecuting and/or taking civil proceedings to recover damages against those responsible</p>
+<h2>Confidentiality</h2>
+ <p> We are registered under the Data Protection Act 1998 and as such, any information concerning the Client and their respective Client Records may be passed to third parties. However, Client records are regarded as confidential and therefore will not be divulged to any third party, other than [our manufacturer/supplier(s) and] if legally required to do so to the appropriate authorities. Clients have the right to request sight of, and copies of any and all Client Records we keep, on the proviso that we are given reasonable notice of such a request. Clients are requested to retain copies of any literature issued in relation to the provision of our services. Where appropriate, we shall issue Client&rsquo;s with appropriate written information, handouts or copies of records as part of an agreed contract, for the benefit of both parties.</p>
+ 
+<p><strong>We will not sell, share, or rent your personal information to any third party or use your e-mail address for unsolicited mail. Any emails sent by this Company will only be in connection with the provision of agreed services and products.</strong></p>
+
+<h2>Disclaimer</h2>
+<p><strong>Exclusions and Limitations</strong><strong> </strong><br />
+  The information on this web site is provided on an &quot;as is&quot; basis. To the fullest extent permitted by law, this Company:</p>
+<ul>
+  <li>excludes all representations and warranties relating to this website and its contents or which is or may be provided by any affiliates or any other third party, including in relation to any inaccuracies or omissions in this website and/or the Company&rsquo;s literature; and </li>
+  <li>excludes all liability for damages arising out of or in connection with your use of this website. This includes, without limitation, direct loss, loss of business or profits (whether or not the loss of such profits was foreseeable, arose in the normal course of things or you have advised this Company of the possibility of such potential loss), damage caused to your computer, computer software, systems and programs and the data thereon or any other direct or indirect, consequential and incidental damages. </li>
+</ul>
+<p>This Company does not however exclude liability for death or personal injury caused by its negligence. The above exclusions and limitations apply only to the extent permitted by law. None of your statutory rights as a consumer are affected. </p>
+
+<h2>Payment</h2>
+<p>Cash or Personal Cheque with Bankers Card, all major Credit/Debit Cards, Bankers Draft or BACS Transfer are all acceptable methods of payment. Our Terms are payment in full within thirty days. All goods remain the property of the Company until paid for in full. Monies that remains outstanding by the due date will incur late payment interest at the rate of 2% above the prevailing Bank of England's base rate on the outstanding balance until such time as the balance is paid in full and final settlement. We reserve the right to seek recovery of any monies remaining unpaid sixty days from the date of invoice via collection Agencies and/or through the Small Claims Court in the event that the outstanding balance does not exceed £3000. In such circumstances, you shall be liable for any and all additional administrative and/or court costs. [Amend as appropriate] </p>
+<p>Returned cheques will incur a £25 charge to cover banking fees and administrative costs. In an instance of a second Returned cheque, we reserve the right to terminate the arrangement and, if agreed to, we shall insist on future cash transactions only. Consequently, all bookings and/or transactions and agreements entered into will cease with immediate effect until such time as any and all outstanding monies are recovered in full.   </p>
+
+<h2>Cancellation Policy</h2>
+  <p>Minimum 24 hours notice of cancellation required. Notification for instance, in person, via email, mobile phone &lsquo;text message&rsquo; and/or fax, or any other means will be accepted subject to confirmation in writing. We reserve the right to levy a £30 charge to cover any subsequent administrative expenses.</p>
+
+<h2>Termination of Agreements and Refunds Policy</h2>
+  <p>Both the Client and ourselves have the right to terminate any Services Agreement for any reason, including the ending of services that are already underway. No refunds shall be offered, where a Service is deemed to have begun and is, for all intents and purposes, underway. Any monies that have been paid to us which constitute payment in respect of the provision of unused Services, shall be refunded.</p>
+
+<h2>Availability</h2>
+  <p>Unless otherwise stated, the services featured on this website are only available within the United Kingdom, or in relation to postings from the United Kingdom. All advertising is intended solely for the United Kingdom market. You are solely responsible for evaluating the fitness for a particular purpose of any downloads, programs and text available through this site. Redistribution or republication of any part of this site or its content is prohibited, including such by framing or other similar or any other means, without the express written consent of the Company. The Company does not warrant that the service from this site will be uninterrupted, timely or error free, although it is provided to the best ability. By using this service you thereby indemnify this Company, its employees, agents and affiliates against any loss or damage, in whatever manner, howsoever caused.<strong> </strong></p>
+
+<h2>Log Files</h2>
+<p>We use IP addresses to analyse trends, administer the site, track user&rsquo;s movement, and gather broad demographic information for aggregate use. IP addresses are not linked to personally identifiable information. Additionally, for systems administration, detecting usage patterns and troubleshooting purposes, our web servers automatically log standard access information including browser type, access times/open mail, URL requested, and referral URL. This information is not shared with third parties and is used only within this Company on a need-to-know basis. Any individually identifiable information related to this data will never be used in any way different to that stated above without your explicit permission.</p>
+
+<h2>Cookies</h2>
+<p>Like most interactive web sites this Company&rsquo;s website [or ISP] uses cookies to enable us to retrieve user details for each visit. Cookies are used in some areas of our site to enable the functionality of this area and ease of use for those people visiting. Some of our affiliate partners may also use cookies. [If you do not use cookies, delete this clause]</p>
+
+<h2>Links to this website</h2>
+ <p> You may not create a link to any page of this website without our prior written consent. If you do create a link to a page of this website you do so at your own risk and the exclusions and limitations set out above will apply to your use of this website by linking to it.</p>
+
+<h2>Links from this website</h2>
+ <p> We do not monitor or review the content of other party&rsquo;s websites which are linked to from this website. Opinions expressed or material appearing on such websites are not necessarily shared or endorsed by us and should not be regarded as the publisher of such opinions or material. Please be aware that we are not responsible for the privacy practices, or content, of these sites. We encourage our users to be aware when they leave our site &amp; to read the privacy statements of these sites. You should evaluate the security and trustworthiness of any other site connected to this site or accessed through this site yourself, before disclosing any personal information to them. This Company will not accept any responsibility for any loss or damage in whatever manner, howsoever caused, resulting from your disclosure to third parties of personal information.</p>
+
+<h2>Copyright Notice</h2>
+ <p> Copyright and other relevant intellectual property rights exists on all text relating to the Company&rsquo;s services and the full content of this website. </p>
+<p>This Company&rsquo;s logo is a registered trademark of this Company in the United Kingdom and other countries. The brand names and specific services of this Company featured on this web site are trade marked [delete this paragraphed clause if no registered trademark exists]. </p>
+
+<h2>Communication</h2>
+<p>We have several different e-mail addresses for different queries. These, &amp; other contact information, can be found on our <strong>Contact Us</strong> link on our website or via Company literature or via the Company&rsquo;s stated telephone, facsimile or mobile telephone numbers.</p>
+
+<h2>Force Majeure</h2>
+<p>Neither party shall be liable to the other for any failure to perform any obligation under any Agreement which is due to an event beyond the control of such party including but not limited to any Act of God, terrorism, war, Political insurgence, insurrection, riot, civil unrest, act of civil or military authority, uprising, earthquake, flood or any other natural or man made eventuality outside of our control, which causes the termination of an agreement or contract entered into, nor which could have been reasonably foreseen. Any Party affected by such event shall forthwith inform the other Party of the same and shall use all reasonable endeavours to comply with the terms and conditions of any Agreement contained herein.</p>
+
+<h2>Waiver</h2>
+ <p> Failure of either Party to insist upon strict performance of any provision of this or any Agreement or the failure of either Party to exercise any right or remedy to which it, he or they are entitled hereunder shall not constitute a waiver thereof and shall not cause a diminution of the obligations under this or any Agreement. No waiver of any of the provisions of this or any Agreement shall be effective unless it is expressly stated to be such and signed by both Parties.</p>
+
+<h3>General</h3>
+<p>The laws of England and Wales govern these terms and conditions. By accessing this website [and using our services/buying our products] you consent to these terms and conditions and to the exclusive jurisdiction of the English courts in all disputes arising out of such access. If any of these terms are deemed invalid or unenforceable for any reason (including, but not limited to the exclusions and limitations set out above), then the invalid or unenforceable provision will be severed from these terms and the remaining terms will continue to apply. Failure of the Company to enforce any of the provisions set out in these Terms and Conditions and any Agreement, or failure to exercise any option to terminate, shall not be construed as waiver of such provisions and shall not affect the validity of these Terms and Conditions or of any Agreement or any part thereof, or the right thereafter to enforce each and every provision. These Terms and Conditions shall not be amended, modified, varied or supplemented except in writing and signed by duly authorised representatives of the Company. </p>
+
+<h2>Notification of Changes</h2>
+ <p> The Company reserves the right to change these conditions from time to time as it sees fit and your continued use of the site will signify your acceptance of any adjustment to these terms. If there are any changes to our privacy policy, we will announce that these changes have been made on our home page and on other key pages on our site. If there are any changes in how we use our site customers&rsquo; Personally Identifiable Information, notification by e-mail or postal mail will be made to those affected by this change. Any changes to our privacy policy will be posted on our web site 30 days prior to these changes taking place. You are therefore advised to re-read this statement on a regular basis</p>
+
+<p><strong>These terms and conditions form part of the Agreement between the Client and ourselves. Your accessing of this website and/or undertaking of a booking or Agreement indicates your understanding, agreement to and acceptance, of the Disclaimer Notice and the full Terms and Conditions contained herein. Your statutory Consumer Rights are unaffected.</strong>    <br />
+  © Fate Crew 2014 All Rights Reserved </p>
+
     
 
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at magna lacus. Ut suscipit elementum nunc, ut vestibulum leo auctor nec. Sed justo orci, dignissim a aliquet vitae, blandit eleifend sem. Nam pellentesque vehicula eros at rutrum. Nulla ipsum tortor, sodales sed imperdiet vel, interdum ut velit. Nullam lacinia tellus eget eros hendrerit non interdum est consectetur. Phasellus posuere bibendum luctus. Morbi auctor diam eleifend nunc vestibulum ut viverra turpis sollicitudin. Ut tempor sagittis mi, et feugiat enim suscipit at. Ut porta est sit amet velit rutrum vitae malesuada massa laoreet. Aenean eu risus at ipsum lobortis mattis nec at enim. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
 
-<p>Morbi vitae mauris risus, ac commodo neque. Integer ac bibendum justo. Proin imperdiet accumsan elit sit amet dignissim. Curabitur gravida est eget ipsum vulputate eget adipiscing orci adipiscing. Aliquam sed risus ut leo tincidunt venenatis. Donec in nibh velit, vitae dapibus magna. Morbi tellus sem, varius vitae blandit sed, porttitor vitae tortor. Morbi augue nibh, commodo a tincidunt aliquam, mollis quis mi.</p>
-
-<p>Donec non sem id sem mattis aliquam. Suspendisse sit amet nunc id neque consequat dignissim. In at quam vel augue dignissim consequat eu eget diam. Aenean scelerisque, odio sed tristique interdum, urna ante dictum diam, nec bibendum leo libero ut lacus. Mauris porta odio diam. Quisque eu leo turpis, et malesuada elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-
-<p>Pellentesque id urna ultricies elit ultricies scelerisque. Nam sit amet nibh libero, ut pulvinar ipsum. Praesent feugiat orci nec enim luctus at porttitor risus auctor. Nullam rutrum placerat neque. Mauris ante nisl, gravida sed consequat vehicula, ultrices et felis. Sed eu nibh nec lacus auctor congue ut quis augue. Phasellus vehicula vehicula egestas. In quis eros sit amet mauris laoreet malesuada et at eros. Fusce volutpat augue ac velit eleifend et cursus lacus pellentesque. Maecenas tellus erat, gravida quis ullamcorper ac, sodales eget dui. Vivamus luctus mattis ligula, quis cursus ipsum condimentum sit amet. Pellentesque dictum molestie est, ac pulvinar dolor faucibus eget. Praesent sit amet lectus id tortor viverra scelerisque sed ac orci.</p>
-
-<p>Morbi lorem magna, accumsan nec sodales id, dignissim vel sem. Nunc quis ligula eros, ac ullamcorper ipsum. Proin fringilla malesuada felis, et dignissim velit sollicitudin sed. Praesent a mattis elit. Aenean sagittis interdum arcu sed eleifend. Pellentesque congue hendrerit est eu vulputate. Curabitur a pellentesque lectus. </p>
-  </div>
+ </div>
   <div class="one_third">
-    <div class="ad">Content for  class "one_third" Goes Here</div>
+    <div class="ad"><a href="http://www.kqzyfj.com/click-7554568-11848784" target="_top">
+<img src="http://www.ftjcfx.com/image-7554568-11848784" width="400" height="600" alt="" border="0"/></a></div>
   </div>
   <div class="clear">&nbsp;</div>
   </div>
