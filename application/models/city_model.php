066d5612576f81eec368634b5646b3af9e0801d7
Chris Evans <chris.evans@balanceinternet.com.au>
2015-01-14 23:46:44 +1100

    added city integration

--- application/models/city_model.php
+++ application/models/city_model.php
@@ -6,6 +6,14 @@ class City_model extends CI_Model {
 	var $date_created	= '';
 	var $date_modified	= '';
 
+	var $longitude 	= '';
+	var $latitude 	= '';
+	var $backgroundImageUrl 	= '';
+	var $timezone 	= '';
+	var $countryName = '';
+	var $state = '';
+	var $city_id_ls = "";
+
 	function __construct() {
 		parent::__construct();
 	}
@@ -46,6 +54,68 @@ class City_model extends CI_Model {
 
 	function delete($id) {
 		$this->db->delete('city', array('id' => $id));
+	}
+
+	function integration_insert($data){
+
+		
+		for($i =0 ;$i<count($data);++$i){
+		
+			if(!is_null($data[$i])){
+
+				// Numbers to skip 9
+
+				$this->name	= $data[$i]['name'];
+				$this->active	= $data[$i]['active'];
+				$this->date_created	= $data[$i]['date_created'];
+				$this->date_modified	= $data[$i]['date_modified'];
+
+				$this->longitude 	= $data[$i]['longitude'];
+				$this->latitude 	= $data[$i]['latitude'];
+				$this->backgroundImageUrl 	= $data[$i]['backgroundImageUrl'];
+				$this->timezone 	= $data[$i]['timezone'];
+				$this->countryName 	= $data[$i]['countryName'];
+				$this->state 	= $data[$i]['state'];
+				$this->city_id_ls = $i+1;
+				$this->addCitySku($this->city_id_ls);
+
+				//echo ".....".$i."....";
+
+				//var_dump($data[$i]);
+
+				try{
+					$this->db->insert('city', $this);
+
+				} catch (Exception $e) {
+	 			   echo 'Caught exception: ',  $e->getMessage(), "\n";
+				}
+			}
+
+		} 
+
+	}
+
+	function addCitySku($city_id){
+
+		// call api
+
+		$data = json_decode(file_get_contents("http://monocle.livingsocial.com/v2/deals?city=".$city_id."&api-key=881E8E5F32BD45D193BD2A03CC2DD213"),true);
+
+
+		// look through SKU 
+
+		for($i=0;$i<count($data['deals']);$i++){
+			$sku =$data['deals'][$i]['id'];
+			// loop through city IDs 
+			for($j=0;$j<count($data['deals'][$i]["city_ids"]);$j++){
+				$city =$data['deals'][$i]["city_ids"][$j];
+				// insert to city
+				$this->db->insert('city_sku', array("sku" =>$sku, "city_id_ls"=>$city));
+			}
+		}
+
+		
+
 	}
 }
 ?>
